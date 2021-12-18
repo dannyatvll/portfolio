42f55ace62117184e934352e1fcf991bb6bd8430
DESKTOP-MH8IQQD\Danny Atiencia <danny24999@gmail.com>
2021-12-18 22:37:22 +0100

    Add First changes

---
+++ src/Components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div className="NavBarPadre">
+      <div className="Secciones">
+        <p>Sobre mi</p>
+        <p>Mi CV</p>
+        <p>Mis proyectos</p>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavBar from "../Components/NavBar";
+
+export const LandingPage = () => {
+  return (
+        <NavBar/>
+  );
+};
+
+export default LandingPage;
